1152baaf7bfc5c88ba9aa8d29958ebc4aac24048
Ironowl1907 <facuguialtana7@gmail.com>
2026-04-10 11:52:24 +0000

    Adding graaf

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,14 @@ int main0() {
 
 int main(void)
 {
-    InitWindow(800, 450, "raylib example - basic window");
+    InitWindow(1000, 1000, "raylib example - basic window");
+		SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            DrawText("Congrats! You created your first window!", 0, 0, 20, LIGHTGRAY);
         EndDrawing();
     }
 
